e4571cab72470f48213f6f289d0d1fbc3555b639
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-05-18 23:34:57 +0300

    initial commit

---
+++ src/main/java/ru/practicum/user/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package ru.practicum.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
